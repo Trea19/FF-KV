698d8e68f41f4cb941863a65b631a600f21cf97b
Trea19 <1242734867@qq.com>
2023-03-08 15:15:42 +0800

    add btree put get delet

--- index/btree.go
+++ index/btree.go
@@ -13,13 +13,36 @@ type BTree struct {
 }
 
 func (bt *BTree) Put(key []byte, pos *data.LogRecordPos) bool {
+	it := &Item{key: key, pos: pos}
+	bt.lock.Lock()
 
+	bt.tree.ReplaceOrInsert(it)
+
+	bt.lock.Unlock()
+	return true
 }
 
 func (bt *BTree) Get(key []byte) *data.LogRecordPos {
+	it := &Item{key: key}
+	btreeItem := bt.tree.Get(it)
 
+	if btreeItem == nil {
+		return nil
+	}
+
+	return btreeItem.(*Item).pos
 }
 
 func (bt *BTree) Delete(key []byte) bool {
+	it := &Item{key: key}
+	bt.lock.Unlock()
 
+	oldItem := bt.tree.Delete(it)
+
+	bt.lock.Unlock()
+	if oldItem == nil {
+		return false
+	}
+
+	return true
 }
--- index/index.go
+++ index/index.go
@@ -1,9 +1,23 @@
 package index
 
-import "bitcask-go/data"
+import (
+	"bitcask-go/data"
+	"bytes"
+
+	"github.com/google/btree"
+)
 
 type Indexer interface {
 	Put(key []byte, pos *data.LogRecordPos) bool
 	Get(key []byte) *data.LogRecordPos
 	Delete(key []byte) bool
+}
+
+type Item struct {
+	key []byte
+	pos *data.LogRecordPos
+}
+
+func (ai *Item) Less(bi btree.Item) bool {
+	return bytes.Compare(ai.key, bi.(*Item).key) == -1
 }
