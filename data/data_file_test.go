6d5d8ad579adf86c48b7c3f599295160d16ad7e2
Trea19 <1242734867@qq.com>
2023-04-12 20:57:50 +0800

    finish ch8

--- data/data_file_test.go
+++ data/data_file_test.go
@@ -21,4 +21,41 @@ func TestOpenDataFile(t *testing.T) {
 	assert.NotNil(t, dataFile3)
 }
 
-// TODO 8-3840
+func TestDataFile_Write(t *testing.T) {
+	dataFile, err := OpenDataFile(os.TempDir(), 0)
+	assert.Nil(t, err)
+	assert.NotNil(t, dataFile)
+
+	err = dataFile.Write([]byte("aaa"))
+	assert.Nil(t, err)
+
+	err = dataFile.Write([]byte("bbb"))
+	assert.Nil(t, err)
+
+	err = dataFile.Write([]byte("ccc"))
+	assert.Nil(t, err)
+}
+
+func TestDataFile_Close(t *testing.T) {
+	dataFile, err := OpenDataFile(os.TempDir(), 0)
+	assert.Nil(t, err)
+	assert.NotNil(t, dataFile)
+
+	err = dataFile.Write([]byte("aaa"))
+	assert.Nil(t, err)
+
+	err = dataFile.Close()
+	assert.Nil(t, err)
+}
+
+func TestDataFile_Sync(t *testing.T) {
+	dataFile, err := OpenDataFile(os.TempDir(), 0)
+	assert.Nil(t, err)
+	assert.NotNil(t, dataFile)
+
+	err = dataFile.Write([]byte("aaa"))
+	assert.Nil(t, err)
+
+	err = dataFile.Sync()
+	assert.Nil(t, err)
+}
